dc0db5b00514d56747e799e51743f8bdbb0baad4
rparth61 <rparth61@gmail.com>
2026-01-14 11:35:52 +0530

    commit

---
+++ src/test/java/base/BaseTest.java
@@ -0,0 +1,28 @@
+package base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.*;
+
+import java.time.Duration;
+
+public class BaseTest {
+    protected WebDriver wd;
+
+    @BeforeClass
+    public void setup() {
+        wd = new EdgeDriver();
+        wd.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        wd.get("https://www.orangehrm.com/en/contact-sales");
+        wd.manage().window().maximize();
+    }
+
+    @AfterClass
+    public void tearDown() {
+            if (wd != null){
+                wd.quit();
+            }
+
+    }
+
+}
---
+++ src/test/java/tests/ContactSalesTest.java
@@ -0,0 +1,23 @@
+package tests;
+
+import base.BaseTest;
+import org.testng.annotations.Test;
+import pages.ContactSalesPage;
+import utils.TestDataProvider;
+
+public class ContactSalesTest extends BaseTest {
+
+    @Test(dataProvider = "contactSalesData", dataProviderClass = TestDataProvider.class)
+    public void submitUsingExcel(String name, String email, String phone, String country,
+                                 String company, String job, String emp, String comment) {
+        ContactSalesPage page = new ContactSalesPage(wd);
+
+        page.acceptCookiesIfPresent();
+        page.fillForm(name, email, phone, company, job, comment);
+        page.selectDropDownByValue(country,emp);
+        page.clickCaptchaCheckbox();
+        page.waitForCaptchaToBeSolved();
+        page.submitForm();
+        page.isSubmissionSuccessful();
+    }
+}
